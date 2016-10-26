1841ee08c79d6a54433820b28352e5f2c4ef0a12
howdyhi <money@AnonymousdeMacBook-Air.local>
2016-10-26 20:55:29 +0800

    draw flowers

--- app/src/main/java/yzu/money/drawflowers/myview.java
+++ app/src/main/java/yzu/money/drawflowers/myview.java
@@ -7,18 +7,36 @@ import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+
 /**
  * Created by money on 2016/10/23.
  */
+class Flower{
+    float x;
+    float y;
+
+    public Flower(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class myview extends View{
     float x = 0, y = 0;
     float petalR;//petal's radius
 
+    ArrayList<Flower> flowers = new ArrayList<Flower>();
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         x = event.getX();
         y = event.getY();
+
+        Flower flower=new Flower(x,y);
+
+        flowers.add(flower);
+
         invalidate();
         return super.onTouchEvent(event);
     }
@@ -30,24 +48,28 @@ public class myview extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         //create paint
         Paint p = new Paint();
         //set petal's radius
         petalR = 15;
 
-        //draw petals
-        p.setColor(Color.parseColor("#f1b1f9"));//set paint's color
-        canvas.drawCircle(x+35, y, petalR, p); //r
-        canvas.drawCircle(x+25, y+25, petalR, p); //r b
-        canvas.drawCircle(x, y+35, petalR, p); //b
-        canvas.drawCircle(x-25, y+25, petalR, p); //b l
-        canvas.drawCircle(x-35, y, petalR, p); //l
-        canvas.drawCircle(x-25, y-25, petalR, p); //t l
-        canvas.drawCircle(x, y-35, petalR, p);//t
-        canvas.drawCircle(x+25, y-25, petalR, p);// t r
-        //draw mid circle
-        p.setColor(Color.YELLOW);
-        canvas.drawCircle(x, y, 25, p);
+            for(int i=0; i<flowers.size(); i++)//redraw
+            {
+                //draw petals
+                p.setColor(Color.parseColor("#f1b1f9"));//set paint's color
+                canvas.drawCircle(flowers.get(i).x+35, flowers.get(i).y,petalR,p);
+                canvas.drawCircle(flowers.get(i).x+25, flowers.get(i).y+25, petalR, p); //r b
+                canvas.drawCircle(flowers.get(i).x, flowers.get(i).y+35, petalR, p); //b
+                canvas.drawCircle(flowers.get(i).x-25, flowers.get(i).y+25, petalR, p); //b l
+                canvas.drawCircle(flowers.get(i).x-35, flowers.get(i).y, petalR, p); //l
+                canvas.drawCircle(flowers.get(i).x-25, flowers.get(i).y-25, petalR, p); //t l
+                canvas.drawCircle(flowers.get(i).x, flowers.get(i).y-35, petalR, p);//t
+                canvas.drawCircle(flowers.get(i).x+25, flowers.get(i).y-25, petalR, p);// t r
+                //draw mid circle
+                p.setColor(Color.YELLOW);
+                canvas.drawCircle(flowers.get(i).x, flowers.get(i).y, 25, p);
+            }
 
     }
 }
