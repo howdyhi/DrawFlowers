f96e967f0634565fef2e2ccc68b074626f7b4665
howdyhi <money@161-16.wlan.yzu.edu.tw>
2016-10-27 10:35:06 +0800

    btn

--- app/src/main/java/yzu/money/drawflowers/MainActivity.java
+++ app/src/main/java/yzu/money/drawflowers/MainActivity.java
@@ -2,6 +2,8 @@ package yzu.money.drawflowers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new myview(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0,0,0,"關於");
+        return true;
+        //getMenuInflater().inflate(R.menu.m);
+        //return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case 0:
+                myvie
+
+        }
+        //return super.onOptionsItemSelected(item);
     }
 }
